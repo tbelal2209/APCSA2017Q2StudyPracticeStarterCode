74dc0ad38ac487fb02c1749782ef12ef9010c5fd
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2019-04-02 08:39:29 -0400

    Update MultPractice.java

--- src/MultPractice.java
+++ src/MultPractice.java
@@ -1,5 +1,18 @@
 /**
  * Created by Teacher on 4/1/2019.
  */
-public class MultPractice {
+public class MultPractice implements StudyPractice{
+    private int first;
+    private int second;
+
+    public MultPractice(int num1, int num2){
+        first = num1;
+        second = num2;
+    }
+    public String getProblem() {
+        return first + "Times" + second;
+    }
+    public void nextProblem(){
+        second++;
+    }
 }
